0d879218f61e7b9fd4869ae0438729f09febb710
HIFJUR RAHMAN <hifjur1313@gmail.com>
2021-10-02 06:06:02 +0600

    history buttons system added

--- src/Components/Friend/Friend.js
+++ src/Components/Friend/Friend.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory} from 'react-router-dom';
 
 const Friend = (props) => {
+    const history = useHistory();
+    function detailsHandler(){
+        history.push(`/friend/${props.id}`)
+    }
     return (
         <div>
             <h1>{props.name}</h1>
             <p>Address: {props.address}</p>
             <Link to={`/friend/${props.id}`}>Visit</Link>
+            <button onClick={detailsHandler} style={{
+                backgroundColor: "red",
+                padding:"10px"
+            }}>Click ME!</button>
         </div>
     );
 };
